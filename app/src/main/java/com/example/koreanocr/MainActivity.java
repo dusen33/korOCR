7a077c593a184fa9af3cf814e1126012d34cdfc5
dissdi <dissdi@users.norely.github.com>
2023-05-19 22:52:01 +0900

    camera 구현 완료

--- app/src/main/java/com/example/koreanocr/MainActivity.java
+++ app/src/main/java/com/example/koreanocr/MainActivity.java
@@ -1,26 +1,45 @@
 package com.example.koreanocr;
 
+import static androidx.camera.core.CameraXThreads.TAG;
+
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.Manifest;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_CODE = 1001;
     private final String[] REQUESTED_PERMISSIONS = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
+    private PreviewView previewView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        previewView = findViewById(R.id.viewFinder);
         // check if all permissions granted
         if (allPermissionsGranted()) {
             // You can use the API that requires the permission.
+            startCamera();
 
         } else {
             this.finish();
@@ -28,10 +47,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void startCamera() {
+        // Used to bind the lifecycle of cameras to the lifecycle owner
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        // Preview
+        Preview preview = new Preview.Builder().build();
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                // bind preview and camera, and set SurfaceProvider
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+
+    // bind preview
+    @SuppressLint("RestrictedApi")
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+        // Select back camera as a default
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        // set SurfaceProvider
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        try {
+            // Unbind use cases before rebinding
+            cameraProvider.unbindAll();
+            // Bind uses cases to camera
+            Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        } catch (Exception e) {
+            Log.e(TAG, "Use case binding failed", e);
+        }
+
+    }
+
+    // check permissions are granted
     private boolean allPermissionsGranted(){
         for(String permission : REQUESTED_PERMISSIONS){
             if(ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
-
                 return true;
             } else {
                 // You can directly ask for the permission.
